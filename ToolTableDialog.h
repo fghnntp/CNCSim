ec515c3621960256271aaa1b2b1d79cefce03452
ring <fghnntp@hotmail.com>
2025-06-30 13:58:13 +0800

    Add milltask.

---
+++ ToolTableDialog.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <QDialog>
+#include <QTableWidget>
+#include <QPushButton>
+#include "tool_manager.h"
+
+class ToolTableDialog : public QDialog {
+    Q_OBJECT
+public:
+    ToolTableDialog(ToolManager *toolManger, QWidget* parent = nullptr);
+
+private slots:
+    void onAddTool();
+    void onEditTool();
+    void onRemoveTool();
+    void refreshTable();
+
+private:
+    ToolManager* toolManager_;
+    QTableWidget* tableWidget_;
+    QPushButton* addButton_;
+    QPushButton* editButton_;
+    QPushButton* removeButton_;
+};
